7bfd7bf7f06d50b4216a2a83f0423c5fef87b871
tienbot1 <tien.ru@mail.ru>
2023-04-06 12:56:28 +0900

    select arrow

--- script.js
+++ script.js
@@ -10,14 +10,24 @@ function renderCard(data){
 
 function createCard(obj) {
     const container = document.body.querySelector("section").querySelector(".container")
+    //создаем карточку
     const card = document.createElement('div')
     card.className = "card"
-
+    //создаем картинку
     const img = document.createElement('img')
     img.setAttribute("src", obj.image)
-
-    const card_text = document.createElement('div')
+    //создаем текстовый блок
+    let card_text = document.createElement('div')
     card_text.className = "card__text"
+
+    // card_text.innerHtml = `
+    //     <p class="name">${obj.name}</p>
+    //     <p>Actor: ${obj.actor}</p>
+    //     <p>Gender: ${obj.gender}</p>
+    //     <p>House: ${obj.house}</p>
+    //     <p>Wand core: ${obj.wand.core}</p>
+    //     <p>Alive: yes</p>`
+
     const name = document.createElement('p')
     name.className = "name"
     name.innerText = obj.name
@@ -26,14 +36,13 @@ function createCard(obj) {
     const gender = document.createElement('p')
     gender.innerText = `Gender: ${obj.gender}`
     const house = document.createElement('p')
-    house.innerText = `House: ${obj.gender}`
+    house.innerText = `House: ${obj.house}`
     const wand = document.createElement('p')
     wand.innerText = `Wand core: ${obj.wand.core}`
     const alive = document.createElement('p')
     obj.alive ? alive.innerText = `Alive: yes` : alive.innerText = `Alive: no`
 
-
-    container.append(card)
-    card.append(img, card_text)
-    card_text.append(name, actor, gender, house, wand, alive)
+    container.append(card) //выводим карточку
+    card.append(img, card_text) //выводим картинку и текстовый блок
+    card_text.append(name, actor, gender, house, wand, alive) //выводим содержимое текстового блока
 }
